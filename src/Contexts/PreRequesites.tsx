4a3e871c13fe85f9469b7145c1906a3f4e6d5dfe
Henrique <henrique@ejcm.com.br>
2023-11-15 12:53:58 -0300

    feature: criação do contexto que engloba a aplicação.

---
+++ src/Contexts/PreRequesites.tsx
@@ -0,0 +1,48 @@
+import React, { useContext, useState } from "react";
+import { createContext } from 'react';
+
+type SubjectContextProviderProps = {
+    children: React.ReactNode;
+};
+
+type SubjectContextProps = {
+    lockIds : string[],
+    setLockIds : React.Dispatch<React.SetStateAction<string[]>>,
+    currentSubject : number,
+    setCurrentSubject : React.Dispatch<React.SetStateAction<number>>,
+    completed : number[],
+    setCompleted : React.Dispatch<React.SetStateAction<number[]>>,
+}
+
+export const SubjectContext = createContext<SubjectContextProps | null>(null);
+
+export default function SubjectContextProvider({ children } : SubjectContextProviderProps) {
+
+    const [lockIds, setLockIds] = useState<string[]>([]);
+    const [currentSubject, setCurrentSubject] = useState<number>(0);
+    const [completed, setCompleted] = useState<number[]>(
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    );
+    
+    return (
+        <SubjectContext.Provider
+            value={{
+                lockIds,
+                setLockIds,
+                currentSubject,
+                setCurrentSubject,
+                completed,
+                setCompleted
+            }}>
+            {children}
+        </SubjectContext.Provider>
+    )
+}
+
+export function useSubjectContext() {
+    const context = useContext(SubjectContext);
+    if (!context) {
+        throw new Error("Context Issues");
+    }
+    return context;
+}
