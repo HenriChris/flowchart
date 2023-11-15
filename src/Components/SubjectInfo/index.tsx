e8d3782a741ee5ddc4f509a1a6e7a84568a97aae
Henrique <henrique@ejcm.com.br>
2023-11-15 12:52:28 -0300

    feature: criação do componente subjectinfo.

---
+++ src/Components/SubjectInfo/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Container } from "./style";
+
+export type SubjectInfoData = {
+    id: number,
+    name: string,
+    preRequisites : string[],
+    credits : number,
+    content : string,
+    difficultyLevel : string,
+    exams : string,
+    teachers : string,
+    advice : string,
+};
+
+function SubjectInfo (props : SubjectInfoData) {
+    return (
+        <Container>
+            <p>Nome : {props.name}</p>
+            <p>Créditos : {props.credits}</p>
+            <p>Nível de dificuldade : {props.difficultyLevel}</p>
+            <p>Estilo de avaliação : {props.exams}</p>
+            <p>Professores atuais : {props.teachers}</p>
+            <p>Ementa : {props.content}</p>
+            <p>Dica : {props.advice}</p>
+        </Container>
+    )
+};
+
+export default SubjectInfo;
